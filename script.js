15260a8b88471662013c30f9008437a32d6cdba4
pathummalsara93-ux <pathummalsara93@gmail.com>
2025-12-27 16:07:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Firebase Config
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD0szKaFT1THw5HaR3--uE4o3Il8TlxTlo",
+  authDomain: "chatforall-86963.firebaseapp.com",
+  projectId: "chatforall-86963",
+  storageBucket: "chatforall-86963.appspot.com",
+  messagingSenderId: "968673332926",
+  appId: "1:968673332926:web:6789623a796a769924d8ef",
+  measurementId: "G-WX4SKF94PX"
 };
 
 // Initialize Firebase
